e898e6951b640d02fcbcd4849869ac7940fbf43c
Jacobawesome82 <37599517+Jacobawesome82@users.noreply.github.com>
2019-01-17 11:48:01 -0700

    Cleaned up the code

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
-
 import random
 speed(0)
 penup()
 setposition(random.randint(-150, 150), random.randint(-150, 150))
 pendown()
+
 def next_circle():
     left(270)
     penup()
@@ -11,34 +11,39 @@ def next_circle():
     pendown()
     left(90)
 radius=25
+
+def player_guess(player):
+    print(player)
+    xvar = int(input("try to hit the center! on X "))
+    yvar= int(input("try to hit the center! on Y  "))
+    return xvar, yvar
+
+def place_dart(x, y, p_color):
+    color(p_color)
+    setposition(x, y - 3)
+    pendown()
+    begin_fill()
+    circle(6)
+    end_fill()
+    penup()
+
 for i in range (4):
     circle(radius)
     radius += 25
     next_circle()
+
 penup()
 setposition(-754, -723)
 dart_color_one = input("What color of darts do you want player 1?  ")
 dart_color_two = input("what color of darts do you want player 2?  ")
-print("player 1")
-dart_x = int(input("try to hit the center! on X "))
-dart_y= int(input("try to hit the center! on Y  "))
+
+p1_x, p1_y = player_guess("player 1")
+
+p2_x, p2_y = player_guess("player 2")
+
+place_dart(p1_x, p1_y, dart_color_one)
+
+place_dart(p2_x, p2_y, dart_color_two)
+
 penup()
-print("player 2")
-dart_a = int(input("try to hit the center! on X "))
-dart_z= int(input("try to hit the center! on Y  "))
-penup()
-color(dart_color_one)
-setposition(dart_x, dart_y+3)
-pendown()
-begin_fill()
-circle(6)
-end_fill()
-penup()
-color(dart_color_two)
-setposition(dart_x, dart_y-3)
-pendown()
-begin_fill()
-circle(6)
-end_fill()
-penup()
-setposition(990, 909)
+setposition(-210, -210)
